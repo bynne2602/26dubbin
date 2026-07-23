16949a0bf13b2dd136cc6cc5c46c38f5429d871b
bynne2602 <bynne2602@gmail.com>
2026-07-23 09:27:32 +0700

    chore: remove netlify configurations

--- netlify/functions/server.js
+++
@@ -1,6 +0,0 @@
-const server = require('../../dist/server.cjs');
-
-exports.handler = async (event, context) => {
-  // Chuyển tiếp các yêu cầu HTTP từ Netlify Functions vào server Express/Node.js của bạn
-  return server(event, context);
-};
